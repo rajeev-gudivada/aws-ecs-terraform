aa1c927b7e7d459b4071f2f7c151602c9225753d
rajeev-gudivada <117442064+rajeev-gudivada@users.noreply.github.com>
2023-01-04 09:11:26 +0530

    Update output.tf

--- modules/ecs/output.tf
+++ modules/ecs/output.tf
@@ -2,3 +2,9 @@ output "ecs_sg" {
   description = "SG attached to the ECS"
   value       = aws_security_group.service_security_group.id
 }
+
+
+output "alb_arn" {
+  description = "alb id"
+  value       = aws_alb.application_load_balancer.arn
+}
